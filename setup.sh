b76445ebfd39baecf939fb30babc7f2d10b75f6d
Patrick Wallace <paddy3450@gmail.com>
2023-08-24 22:16:52 +1000

    setup script working now

--- setup.sh
+++ setup.sh
@@ -1,138 +1,247 @@
 #!/bin/bash
+
 cd ~
-if [[ ! -d "~/dotfiles" ]]; then
+if [ ! -e ~/dotfiles ]; then
 	git clone http://github.com/paddy3450/dotfiles
+	echo "cloning dotfiles repo"
 fi
-if [[ ! -d "~/Programs" ]]; then
-	mkdir "~/Programs"
-fi
-if [[ ! -d "~/Programs/dwm" ]]; then
-	cd ~/Programs
-	git clone http://github.com/paddy3450/dwm
-	cd ~
-fi
-if [[ ! -d "~/Programs/st" ]]; then
-	cd ~/Programs
-	git clone http://github.com/paddy3450/st
-	cd ~
+
+# environment detection
+if [ -f /etc/arch-release ]; then
+	export MY_INSTALLER="pacman"
+	export MY_INSTALL="-S --noconfirm --needed"
+	$MY_INSTALLER -Syu --noconfirm
+elif [ command -v apt-get ]; then
+	export MY_INSTALLER="apt-get"
+	export MY_INSTALL="-qq install"
+	$MY_INSTALLER update
+elif [ command -v dnf ]; then
+	export MY_INSTALLER="dnf"
+	export MY_INSTALL="-y install"
+	$MY_INSTALLER -y update
 fi
 
 # Move config files in using stow
-if [[ -f "~/.bashrc" && [! -L "~/.bashrc"]]]; then
+if [ -e ~/.bashrc ] && [ ! -L ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc.backup
 fi
-if [[ -f ~/.bash_profile  && [! -L ~/.bash_profile]]]; then
+if [ -e ~/.bash_profile ] && [ ! -L ~/.bash_profile ]; then
 	mv ~/.bash_profile ~/.bash_profile.backup
 fi
 cd ~/dotfiles
 stow -v -R -t ~ home  
 cd ~
 
-# environment detection
-if [ -f /etc/arch-release ]; then
-	export MY_INSTALLER="pacman"
-	expoft MY_INSTALL="-S --noconfirm"
-	MY_INSTALLER -Syu --noconfirm
-elif [ command -v apt-get ]; then
-	export MY_INSTALLER="apt-get"
-	expoft MY_INSTALL="-qq install"
-	MY_INSTALLER update
-elif [ command -v dnf ]; then
-	export MY_INSTALLER="dnf"
-	expoft MY_INSTALL="-y install"
-	MY_INSTALLER -y update
-fi
  
+function install_dwm(){
+	if [ ! -e ~/Programs ]; then
+		mkdir Programs
+		echo "MAKING PROGRAMS FOLDER"
+	fi
+	if [ ! -e ~/Programs/dwm ]; then
+		cd ~/Programs
+		echo "cloning DWM from github"
+		git clone http://github.com/paddy3450/dwm
+		cd ~/Programs/dwm
+		sudo make clean install
+		cd ~
+	fi
+}
+
+function install_st(){
+	if [ ! -e ~/Programs ]; then
+		mkdir Programs
+		echo "MAKING PROGRAMS FOLDER"
+	fi
+	if [ ! -e ~/Programs/st ]; then
+		cd ~/Programs
+		echo "cloning ST from github"
+		git clone http://github.com/paddy3450/st
+		cd ~/Programs/st
+		sudo make clean install
+		cd ~
+	fi
+}
+
+function install_minecraft(){
+	if [ ! -e ~/Programs ]; then
+		mkdir Programs
+		echo "MAKING PROGRAMS FOLDER"
+	fi
+	if [ ! -e ~/Programs/minecraft-launcher ]; then
+		cd ~/Programs
+		echo "cloning ST from github"
+		git clone https://aur.archlinux.org/minecraft-launcher.git
+		cd ~/Programs/minecraft-launcher
+		makepkg -si
+		cd ~
+		sudo $MY_INSTALLER $MY_INSTALL gnome-keyring
+	fi
+}
+
 
 # install programs 
+function install_base_utility() {
 # utility
-MY_INSTALLER MY_INSTALL git
-MY_INSTALLER MY_INSTALL sed
-MY_INSTALLER MY_INSTALL stow
-MY_INSTALLER MY_INSTALL feh 
-MY_INSTALLER MY_INSTALL atool 
-MY_INSTALLER MY_INSTALL zip 
-MY_INSTALLER MY_INSTALL gzip 
-MY_INSTALLER MY_INSTALL p7zip
-MY_INSTALLER MY_INSTALL htop
-MY_INSTALLER MY_INSTALL btop
-MY_INSTALLER MY_INSTALL mpc
-MY_INSTALLER MY_INSTALL mpd
-MY_INSTALLER MY_INSTALL mpv
-MY_INSTALLER MY_INSTALL ncmpcpp
-MY_INSTALLER MY_INSTALL vim
-MY_INSTALLER MY_INSTALL neofetch
-MY_INSTALLER MY_INSTALL neovim
-MY_INSTALLER MY_INSTALL network-manager-applet 
-MY_INSTALLER MY_INSTALL networkmanager 
-MY_INSTALLER MY_INSTALL networkmanager-openvpn 
-MY_INSTALLER MY_INSTALL openvpn
-MY_INSTALLER MY_INSTALL nnn
-MY_INSTALLER MY_INSTALL tmux
-MY_INSTALLER MY_INSTALL rclone
-MY_INSTALLER MY_INSTALL vlc
-MY_INSTALLER MY_INSTALL wmname
-MY_INSTALLER MY_INSTALL openssh
-MY_INSTALLER MY_INSTALL cava
-MY_INSTALLER MY_INSTALL playerctl
-MY_INSTALLER MY_INSTALL pulseaudio pulseaudio-alsa pamixer alsa-utils pavumixer blueberry
-MY_INSTALLER MY_INSTALL pyright python-pandas python-pynvim
-MY_INSTALLER MY_INSTALL pandoc
-#utility graphical
-MY_INSTALLER MY_INSTALL zathura zathura-pdf-mupdf zathura-djvu
-MY_INSTALLER MY_INSTALL noto-fonts 
-MY_INSTALLER MY_INSTALL python-pywal
-MY_INSTALLER MY_INSTALL nemo 
-MY_INSTALLER MY_INSTALL nemo-fileroller
-MY_INSTALLER MY_INSTALL lxappearance
-MY_INSTALLER MY_INSTALL arc-solid-gtk-theme materia-gtk-theme adapta-gtk-theme
-MY_INSTALLER MY_INSTALL papirus-icon-theme
-MY_INSTALLER MY_INSTALL transmission-gtk transmission-cli
-MY_INSTALLER MY_INSTALL arandr
+sudo $MY_INSTALLER $MY_INSTALL git
+sudo $MY_INSTALLER $MY_INSTALL sed
+sudo $MY_INSTALLER $MY_INSTALL stow
+sudo $MY_INSTALLER $MY_INSTALL feh 
+sudo $MY_INSTALLER $MY_INSTALL atool 
+sudo $MY_INSTALLER $MY_INSTALL zip 
+sudo $MY_INSTALLER $MY_INSTALL gzip 
+sudo $MY_INSTALLER $MY_INSTALL p7zip
+sudo $MY_INSTALLER $MY_INSTALL htop
+sudo $MY_INSTALLER $MY_INSTALL btop
+sudo $MY_INSTALLER $MY_INSTALL mpc
+sudo $MY_INSTALLER $MY_INSTALL mpd
+sudo $MY_INSTALLER $MY_INSTALL mpv
+sudo $MY_INSTALLER $MY_INSTALL ncmpcpp
+sudo $MY_INSTALLER $MY_INSTALL vim
+sudo $MY_INSTALLER $MY_INSTALL neofetch
+sudo $MY_INSTALLER $MY_INSTALL neovim
+if [ ! -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+		 ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+sudo $MY_INSTALLER $MY_INSTALL network-manager-applet 
+sudo $MY_INSTALLER $MY_INSTALL networkmanager 
+sudo $MY_INSTALLER $MY_INSTALL networkmanager-openvpn 
+sudo $MY_INSTALLER $MY_INSTALL openvpn
+sudo $MY_INSTALLER $MY_INSTALL nnn
+sudo $MY_INSTALLER $MY_INSTALL tmux
+sudo $MY_INSTALLER $MY_INSTALL rclone
+sudo $MY_INSTALLER $MY_INSTALL vlc
+sudo $MY_INSTALLER $MY_INSTALL wmname
+sudo $MY_INSTALLER $MY_INSTALL openssh
+sudo $MY_INSTALLER $MY_INSTALL cava
+sudo $MY_INSTALLER $MY_INSTALL playerctl
+sudo $MY_INSTALLER $MY_INSTALL pandoc
+sudo $MY_INSTALLER $MY_INSTALL pulseaudio pulseaudio-alsa pamixer alsa-utils pavucontrol bluez blueberry
+sudo $MY_INSTALLER $MY_INSTALL pyright python-pandas python-pynvim
+echo "Enable bluetooth service Y/N"
+read -r enable_bluetooth
+case $enable_bluetooth in
+	y)
+	systemctl enable bluetooth.service
+	systemctl start bluetooth.service ;;
+	*) ;;
+esac
+}
 
+function install_graphical_utility() {
+##utility graphical
+sudo $MY_INSTALLER $MY_INSTALL zathura zathura-pdf-mupdf zathura-djvu
+sudo $MY_INSTALLER $MY_INSTALL noto-fonts 
+sudo $MY_INSTALLER $MY_INSTALL python-pywal
+sudo $MY_INSTALLER $MY_INSTALL nemo 
+sudo $MY_INSTALLER $MY_INSTALL nemo-fileroller
+sudo $MY_INSTALLER $MY_INSTALL lxappearance
+sudo $MY_INSTALLER $MY_INSTALL arc-solid-gtk-theme materia-gtk-theme adapta-gtk-theme
+sudo $MY_INSTALLER $MY_INSTALL papirus-icon-theme
+sudo $MY_INSTALLER $MY_INSTALL transmission-gtk transmission-cli
+sudo $MY_INSTALLER $MY_INSTALL arandr
+}
+
+function install_laptop_utilities() {
 # laptop
-MY_INSTALLER MY_INSTALL acpi
+sudo $MY_INSTALLER $MY_INSTALL acpi
+sudo $MY_INSTALLER $MY_INSTALL sof-firmware
+}
 
-# graphical drivers
-MY_INSTALLER MY_INSTALL mesa
-MY_INSTALLER MY_INSTALL lib32-glu
-MY_INSTALLER MY_INSTALL lib32-nvidia-utils
-MY_INSTALLER MY_INSTALL lib32-vulkan-radeon
+function install_graphics_drivers() {
+	echo "hello";
+## graphical drivers
+#sudo $MY_INSTALLER $MY_INSTALL mesa
+#sudo $MY_INSTALLER $MY_INSTALL lib32-glu
+#sudo $MY_INSTALLER $MY_INSTALL lib32-nvidia-utils
+#sudo $MY_INSTALLER $MY_INSTALL lib32-vulkan-radeon
+#sudo $MY_INSTALLER $MY_INSTALL vulkan-intel
+}
 
+function install_xorg() {
 #X desktop
-MY_INSTALLER MY_INSTALL xorg-server 
-MY_INSTALLER MY_INSTALL xorg-xinit 
-MY_INSTALLER MY_INSTALL xorg-xsetroot 
-MY_INSTALLER MY_INSTALL xbindkeys 
-MY_INSTALLER MY_INSTALL dmenu 
-MY_INSTALLER MY_INSTALL picom 
+sudo $MY_INSTALLER $MY_INSTALL xorg-server 
+sudo $MY_INSTALLER $MY_INSTALL xorg-xinit 
+sudo $MY_INSTALLER $MY_INSTALL xorg-xsetroot 
+sudo $MY_INSTALLER $MY_INSTALL xbindkeys 
+sudo $MY_INSTALLER $MY_INSTALL xclip 
+sudo $MY_INSTALLER $MY_INSTALL dmenu 
+sudo $MY_INSTALLER $MY_INSTALL picom 
+}
 
+function install_xfce4() {
 #xfce
-MY_INSTALLER MY_INSTALL xfce4 xfce4-goodies
+sudo $MY_INSTALLER $MY_INSTALL xfce4 xfce4-goodies
+}
 
+function install_main_gui_programs() {
 #applications
-MY_INSTALLER MY_INSTALL blender
-MY_INSTALLER MY_INSTALL calibre
-MY_INSTALLER MY_INSTALL cantata
-MY_INSTALLER MY_INSTALL chromium
-MY_INSTALLER MY_INSTALL discord
-MY_INSTALLER MY_INSTALL firefox
-MY_INSTALLER MY_INSTALL gimp
-MY_INSTALLER MY_INSTALL inkscape
-MY_INSTALLER MY_INSTALL kdenlive
-MY_INSTALLER MY_INSTALL libreoffice-fresh
-MY_INSTALLER MY_INSTALL obs-studio 
-MY_INSTALLER MY_INSTALL qmk
-MY_INSTALLER MY_INSTALL steam-ntive-runtime
-MY_INSTALLER MY_INSTALL texlive-most biber
-MY_INSTALLER MY_INSTALL xournalpp
-MY_INSTALLER MY_INSTALL youtube-dl
-# Virtual machines
-MY_INSTALLER MY_INSTALL vert-manager
-MY_INSTALLER MY_INSTALL qemu
+#sudo $MY_INSTALLER $MY_INSTALL blender
+#sudo $MY_INSTALLER $MY_INSTALL calibre
+#sudo $MY_INSTALLER $MY_INSTALL cantata
+#sudo $MY_INSTALLER $MY_INSTALL chromium
+#sudo $MY_INSTALLER $MY_INSTALL discord
+sudo $MY_INSTALLER $MY_INSTALL firefox
+sudo $MY_INSTALLER $MY_INSTALL gimp
+#sudo $MY_INSTALLER $MY_INSTALL inkscape
+##sudo $MY_INSTALLER $MY_INSTALL kdenlive
+#sudo $MY_INSTALLER $MY_INSTALL libreoffice-fresh
+sudo $MY_INSTALLER $MY_INSTALL obs-studio 
+##sudo $MY_INSTALLER $MY_INSTALL qmk
+echo "ENABLE MULTI LIB IN /etc/pacman.conf"
+sudo $MY_INSTALLER $MY_INSTALL steam-native-runtime
+sudo $MY_INSTALLER $MY_INSTALL discord
+#sudo $MY_INSTALLER $MY_INSTALL texlive-most biber
+sudo $MY_INSTALLER $MY_INSTALL xournalpp
+sudo $MY_INSTALLER $MY_INSTALL youtube-dl
+}
 
-#expressvpn
-#reaper-bin carla
-#spotify
-#vscodium-bin
-#yay 
+function install_virtual_machines() {
+	echo "hello";
+# Virtual machines
+##sudo $MY_INSTALLER $MY_INSTALL vert-manager
+##sudo $MY_INSTALLER $MY_INSTALL qemu
+}
+
+function menu() {
+	clear
+	echo -ne "
+		1)install base utility
+		2)install graphics drivers
+		3)install laptop utilities
+		4)install xorg
+		5)install st
+		6)install dwm
+		7)install graphical utility
+		8)install xfce4
+		9)install main gui_programs
+		10)install virtual machines
+		11)install minecraft
+		0) Exit
+	"
+	read -r ans
+	case $ans in
+		1)install_base_utility ; menu ;;
+		2)install_graphics_drivers; menu ;;
+		3)install_laptop_utilities ; menu ;;
+		4)install_xorg ; menu ;;
+		5)install_st ; menu ;;
+		6)install_dwm ; menu ;;
+		7)install_graphical_utility ; menu ;;
+		8)install_xfce4 ; menu ;;
+		9)install_main_gui_programs ; menu ;;
+		10)install_virtual_machines ; menu ;;
+		11)install_minecraft ; menu ;;
+		*) exit 0 ;;
+	esac
+}
+
+menu
+
+##expressvpn
+##reaper-bin carla
+##spotify
+##vscodium-bin
+##yay 
+
